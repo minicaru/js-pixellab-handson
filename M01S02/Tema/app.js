34caa14cd4cf9f39e0580b015c0f84b0f27d61a7
minicaru <60358626+minicaru@users.noreply.github.com>
2020-05-31 13:12:26 +0300

    tema 02

--- M01S02/Tema/app.js
+++ M01S02/Tema/app.js
@@ -1,1 +1,14 @@
-console.log('Minicaru Alexandru')
+console.log('Minicaru Alexandru');
+
+
+function myScript() {
+  var name = prompt('Bine ai venit, cum te cheama?');
+  console.log(name);
+  var age = prompt('Bine ai venit, cati ani ai?');
+  console.log(name + ' are ' + age + ' ani');
+}
+
+function myFunction() {
+  var person = prompt('Care este numele tau?');
+  console.log(person);
+} 
