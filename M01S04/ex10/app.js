1a1c6b7ad6edd4bdb0c552d077d68f289eaa8898
minicaru <60358626+minicaru@users.noreply.github.com>
2020-07-06 10:06:49 +0300

    curs

--- M01S04/ex10/app.js
+++ M01S04/ex10/app.js
@@ -34,6 +34,70 @@ var person = {
 console.warn('Folosind obiectul person si forEach, afiseaza skillurile din pozitiile impare ale arrayului.');
 var message = '';
 
-person.skills.forEach(function (){
+person.skills.forEach(function (skill, i){
+  if (i % 2 !== 0) {
+    message += skill + ' ';
 
+  }
+  
 });
+
+console.log(message.trim());
+
+console.warn('In mod similar, afiseaza skillurile care contin litera a.');
+message = '';
+
+person.skills.forEach(function (skill) {
+  if (skill.includes('a')) {
+    message += `${skill} `;
+  }
+});
+
+console.log(message.trim());
+
+console.warn('Folosind forEach afiseaza propozitia: "Prietenii mei se numesc xxx yyy, xxx yyy si xxx yyy."');
+message = 'Prietenii mei se numesc ';
+
+person.friends.forEach(function (friend, i, friends) {
+  var punctuation = ', ';
+  var friendsLength = friends.length;
+
+  message += `${friend.name} ${friend.surname}`;
+
+  if (i === friendsLength - 2) {
+    punctuation = ' si ';
+  }
+
+  if (i === friendsLength - 1) {
+    punctuation = '.';
+  }
+
+  message += punctuation;
+});
+
+console.log(message);
+
+console.warn('Folosind forEach, afiseaza numarul total de ani pe care il au persoanele din arrayul friends, doar daca au varsta mai mare decat 30 inclusiv.');
+var totalAge = 0;
+
+person.friends.forEach(function (friend) {
+  if (friend.age >= 30) {
+    totalAge += friend.age;
+  }
+});
+
+console.log(totalAge);
+
+console.warn('Folosind forEach, afiseaza suma anilor de nastere a persoanelor care au varsta impara. ');
+var totalYears = 0;
+
+person.friends.forEach(function(friend){
+  var friendAge = friend.age;
+  var birthYear = (new Date(Date.now()).getFullYear()) - friendAge;
+
+  if (friendAge % 2!== 0) {
+    totalYears += birthYear;
+  }
+});
+
+console.log(totalYears);
